b7bb8710e4fd97abba6143241481696a53d8184c
mvlada87 <mvlada87@users.noreply.github.com>
2019-03-14 23:52:19 +0100

    v3

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 var scrollVertical = 0;
-var distance = 30;
-var scrollSpeed = 15;
+var distance = 50;
+var scrollSpeed = 6;
 
-var checkbox = document.getElementById('navToggle').checked;
+var maxTopScroll;
+
+function calcWinHeight() {
+  var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
+  maxTopScroll = Math.max(document.body.scrollHeight, document.body.offsetHeight,
+    document.documentElement.clientHeight, document.documentElement.scrollHeight,
+    document.documentElement.offsetHeight) - window.innerHeight;
+}
 
 function autoScrollTo(el) {
+  calcWinHeight();
   var currentY = window.pageYOffset;
   var targetY = document.getElementById(el).offsetTop;
-  var bodyHeight = document.body.offsetHeight;
-  var yPos = currentY + window.innerHeight;
-  var animate = setTimeout('autoScrollTo(\'' + el + '\')', scrollSpeed);
+  var animate = setTimeout(function () { autoScrollTo(el); }, scrollSpeed);
 
-  if (yPos > bodyHeight) {
+  if (document.documentElement.scrollTop >= maxTopScroll) {
+    document.documentElement.scrollTop = maxTopScroll;
     clearTimeout(animate);
-  } else {
-    if (currentY < targetY - distance) {
-      scrollVertical = currentY + distance;
-      window.scroll(0, scrollVertical);
-    } else if (currentY > targetY + distance) {
-      scrollVertical = currentY - distance;
-      window.scroll(0, scrollVertical);
-    } else {
-      clearTimeout(animate);
-    }
+    document.getElementById('navToggle').checked = false;
   }
 
-  console.log();
+  if (currentY < targetY - distance) {
+    scrollVertical = currentY + distance;
+    window.scroll(0, scrollVertical);
+  } else if (currentY > targetY + distance) {
+    scrollVertical = currentY - distance;
+    window.scroll(0, scrollVertical);
+  } else {
+    clearTimeout(animate);
+    document.getElementById('navToggle').checked = false;
+  }
+
 }
